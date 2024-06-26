3c12affd71f4d47acb2ada451c14a5eb29a0d158
unknown <paffedzie001@st.ug.edu.gh>
2024-06-26 13:54:03 +0000

    created a Card Componet

---
+++ components/Card.js
@@ -0,0 +1,23 @@
+import {View,Text,StyleSheet,Image} from 'react-native'
+import mastercard from './Card.png'
+
+const Card = (props)=>{
+ return(
+ <View>
+ 
+  <Image style={{right:20,width:300,height:200,justifyContent:'center',margin:7,marginHorizontal:10,borderRadius:25}} source={mastercard}/>
+ 
+
+    
+
+ 
+ 
+
+  
+  
+  </View>
+ )}
+
+
+
+export default Card
